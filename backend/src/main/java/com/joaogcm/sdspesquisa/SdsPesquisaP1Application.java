8c28450f5bc6f3aa2e4acde983a4d402ec68eae6
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2020-09-14 16:46:01 -0300

    Project created

---
+++ backend/src/main/java/com/joaogcm/sdspesquisa/SdsPesquisaP1Application.java
@@ -0,0 +1,11 @@
+package com.joaogcm.sdspesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsPesquisaP1Application {
+	public static void main(String[] args) {
+		SpringApplication.run(SdsPesquisaP1Application.class, args);
+	}
+}
---
+++ backend/src/test/java/com/joaogcm/sdspesquisa/SdsPesquisaP1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaogcm.sdspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsPesquisaP1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
